b4ac545859fb56a3808a026a967d235a66fc5a07
Sander Vallaots <sander.vallaots@heathmont.net>
2025-01-14 11:48:51 +0200

    fix

--- lib/slack/channel_server.ex
+++ lib/slack/channel_server.ex
@@ -9,7 +9,7 @@ defmodule Slack.ChannelServer do
   # Note, these require the following scopes:
   #   `channels:read`, `groups:read`, `im:read`, `mpim:read`
   @default_channel_types "public_channel,private_channel,mpim,im"
-  @batch_size 10
+  @batch_size 100
   @batch_delay 200
 
   # ----------------------------------------------------------------------------
@@ -19,14 +19,13 @@ defmodule Slack.ChannelServer do
   def start_link({token, bot, config}) do
     Logger.info("[Slack.ChannelServer] starting for #{bot.bot_module}...")
 
-    # This should be either channel types or channel IDs
-    channels_or_types = case Keyword.get(config, :types) do
+    channel_types = case Keyword.get(config, :types) do
       nil -> @default_channel_types
       types when is_binary(types) -> types
-      types when is_list(types) -> types  # Keep as list if it's channel IDs
+      types when is_list(types) -> Enum.join(types, ",")
     end
 
-    GenServer.start_link(__MODULE__, {token, bot, channels_or_types}, name: via_tuple(bot))
+    GenServer.start_link(__MODULE__, {token, bot, channel_types}, name: via_tuple(bot))
   end
 
   def join(bot, channel) do
@@ -56,15 +55,7 @@ defmodule Slack.ChannelServer do
 
   @impl true
   def handle_continue(:fetch_channels, state) do
-    channels = case state.channels_or_types do
-      types when is_binary(types) ->
-        # Fetch channels from Slack API if types are provided
-        fetch_channels_from_api(state.token, types)
-      channel_ids when is_list(channel_ids) ->
-        # Use provided channel IDs directly
-        channel_ids
-    end
-
+    channels = fetch_channels(state.token, state.channel_types)
     {:noreply, %{state | pending_channels: channels}, {:continue, :process_channels}}
   end
 
@@ -119,7 +110,7 @@ defmodule Slack.ChannelServer do
     {:via, Registry, {Slack.ChannelServerRegistry, bot}}
   end
 
-  defp fetch_channels_from_api(token, types) when is_binary(types) do
+  defp fetch_channels(token, types) when is_binary(types) do
     "users.conversations"
     |> Slack.API.stream(token, "channels", %{
       types: types,
